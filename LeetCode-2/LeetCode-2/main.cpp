781f9b7302830c6cdda2edd7c1004b98108af68c
ricardooli <ricardooli@tencent.com>
2021-10-11 07:47:40 +0800

    feat: 修改第二题的解法

--- LeetCode-2/LeetCode-2/main.cpp
+++ LeetCode-2/LeetCode-2/main.cpp
@@ -20,34 +20,28 @@ struct ListNode {
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode *head = new ListNode(-1);
-        ListNode *tail = new ListNode(-1);
+        ListNode *head = nullptr, *tail = nullptr;
         int carry = 0;
         while (l1 || l2) {
-            int res1 = l1 ? l1->val : 0;
-            int res2 = l2 ? l2->val : 0;
-            int sum = res1 + res2 + carry;
-            int res = 0;
-            if (sum >= 10) {
-                carry = 1;
-                res = sum % 10;
+            int n1 = l1 ? l1->val: 0;
+            int n2 = l2 ? l2->val: 0;
+            int sum = n1 + n2 + carry;
+            if (!head) {
+                head = tail = new ListNode(sum % 10);
             } else {
-                res = sum;
-                carry = 0;
+                tail->next = new ListNode(sum % 10);
+                tail = tail->next;
             }
-            if (head->val == -1) {
-                head->val = res;
-                tail = head;
-            } else {
-                ListNode *temp = new ListNode(res);
-                tail->next = temp;
-                tail = temp;
+            carry = sum / 10;
+            if (l1) {
+                l1 = l1->next;
             }
-            l1 = l1->next;
-            l2 = l2->next;
+            if (l2) {
+                l2 = l2->next;
+            }
         }
         if (carry > 0) {
-            tail->next = new ListNode(1);
+            tail->next = new ListNode(carry);
         }
         return head;
     }
