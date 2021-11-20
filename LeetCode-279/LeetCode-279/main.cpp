63a16a4c253c31f5e6c97e3799e8b131ca1c54e8
ricardooli <ricardooli@tencent.com>
2021-11-20 21:52:19 +0800

    feat: 新增279题

--- LeetCode-279/LeetCode-279/main.cpp
+++ LeetCode-279/LeetCode-279/main.cpp
@@ -6,9 +6,28 @@
 //
 
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    // 方法1: 动态规划
+    // 状态转移方程：f(i) = 1 + f(i - j * j)
+    int numSquares(int n) {
+        vector<int> f(n+1);
+        for (int i = 1; i <= n; i++) {
+            int minn = INT_MAX;
+            for (int j = 1; j * j <= i; j++) {
+                minn = min(minn, 1 + f[i - j * j]);
+            }
+            f[i] = minn;
+        }
+        return f[n];
+    }
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    Solution s;
+    cout << s.numSquares(12) << endl;
 }
