6e01a3abe06a67dac42b85f75fd07a9c9627e7f0
ricardooli <ricardooli@tencent.com>
2021-12-12 14:24:58 +0800

    新增了一些题目

---
+++ interview/Bigo-1/Bigo-1/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Bigo-1
+//
+//  Created by Ricardo on 2021/11/29.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ interview/Bigo-1/Bigo-1/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Bigo-1
+//
+//  Created by Ricardo on 2021/11/29.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
