50278b68dd5c429ea8c1a396ed9cb4fd345124e8
ricardooli <ricardooli@tencent.com>
2022-02-11 22:44:55 +0800

    feat: 新增 LeetCode-215 题

--- Algorithm/imooc-algorithm/03-Sorting-Advance/Course Code (C++)/07-Quick-Sort-Deal-With-Identical-Keys/main.cpp
+++ Algorithm/imooc-algorithm/03-Sorting-Advance/Course Code (C++)/07-Quick-Sort-Deal-With-Identical-Keys/main.cpp
@@ -32,7 +32,7 @@ int _partition(T arr[], int l, int r){
 // 双路快速排序的partition
 // 返回p, 使得arr[l...p-1] < arr[p] ; arr[p+1...r] > arr[p]
 template <typename T>
-int _partition2(T arr[], int l, int r){
+int _partition2(T arr[], int l, int r) {
 
     // 随机在arr[l...r]的范围中, 选择一个数值作为标定点pivot
     swap( arr[l] , arr[rand()%(r-l+1)+l] );
@@ -40,10 +40,10 @@ int _partition2(T arr[], int l, int r){
 
     // arr[l+1...i) <= v; arr(j...r] >= v
     int i = l+1, j = r;
-    while( true ){
+    while(true) {
         // 注意这里的边界, arr[i] < v, 不能是arr[i] <= v
         // 思考一下为什么?
-        while( i <= r && arr[i] < v )
+        while(i <= r && arr[i] < v)
             i ++;
 
         // 注意这里的边界, arr[j] > v, 不能是arr[j] >= v
@@ -53,17 +53,13 @@ int _partition2(T arr[], int l, int r){
 
         // 对于上面的两个边界的设定, 有的同学在课程的问答区有很好的回答:)
         // 大家可以参考: http://coding.imooc.com/learn/questiondetail/4920.html
-
         if( i > j )
             break;
-
         swap( arr[i] , arr[j] );
-        i ++;
-        j --;
+        i++;
+        j--;
     }
-
     swap( arr[l] , arr[j]);
-
     return j;
 }
 
